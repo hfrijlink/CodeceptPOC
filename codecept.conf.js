a6029bbb2e703829ded99578d5b2f13bd610e522
Hein-Carl Frijlink <hfrijlink@worth.systems>
2019-02-28 13:57:09 +0100

    added config for BrowserStack

--- codecept.conf.js
+++ codecept.conf.js
@@ -4,9 +4,27 @@ exports.config = {
   helpers: {
     WebDriver: {
       url: 'https://worth.systems',
-      browser: 'chrome'
-    }
-  },
+      browser: 'Firefox',
+      host: 'hub-cloud.browserstack.com',
+      user: 'BROWSER_STACK_USER',
+      key: 'BROWSER_STACK_KEY',
+      desiredCapabilities: {
+        project: 'CodeceptPOC',
+        os : 'Windows',
+        os_version : '7',
+        'browserName' : 'Firefox',
+        'browser_version' : '65.0',
+        'browserstack.local' : 'false',
+        'browserstack.selenium_version' : '3.14.0',
+        'browserstack.geckodriver' : '0.23.0',
+      },
+      restart: true,
+      timeouts: {
+        script: 60000,
+        'page load': 10000
+      }
+      },
+    },
   include: {
     I: './steps.js'
   },
